39781ac807999f69b5f8640283430fa6f191eca2
tangky <945541696@qq.com>
2024-03-29 09:52:19 +0800

    init

---
+++ src/test/java/com/tang/atp/AtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.atp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
