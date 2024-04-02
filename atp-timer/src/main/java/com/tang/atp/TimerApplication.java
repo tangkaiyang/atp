325950b0ef2a4b165611a3d94e8c9707f9b3ff3e
tangky <945541696@qq.com>
2024-04-02 11:26:40 +0800

    run run run

--- atp-timer/src/main/java/com/tang/atp/TimerApplication.java
+++ atp-timer/src/main/java/com/tang/atp/TimerApplication.java
@@ -2,23 +2,27 @@ package com.tang.atp;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@EnableAsync
+//@EnableAsync
 @SpringBootApplication
-@EnableSwagger2
-@Log4j2
-@EnableRedisHttpSession
+//@EnableSwagger2
+//@Log4j2
+//@EnableRedisHttpSession
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @ServletComponentScan
 public class TimerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimerApplication.class, args);
-        log.info("-- api-定时任务服务-服务启动成功");
+        System.out.println("-- api-定时任务服务-服务启动成功");
+//        log.info("-- api-定时任务服务-服务启动成功");
     }
 }
